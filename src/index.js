8de8b8a70a4d3572f9158c43340d83ec5eb26884
Praveent696 <praveent696@gmail.com>
2021-08-17 19:58:49 +0530

    added jwt decode

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 /**
- * @author Vipin Kumar
+ * @author Vipin Kumar, Parveen Kumar
  * @summary  Check JWT is expired or not
  * @description A global JWT validator utility check weather expired or not.
  * @public
  */
 
-import jwt_decode from "jwt-decode";
-
 export default function isJwtTokenExpired(token, optionalKey=null) {
     if (typeof(token) !== 'string' || !token) throw new Error('Invalid JWT token');
-
     let IsJwtTokenExpired = false;
-    const decode = jwt_decode(token);
+    const decode =  decodeToken(token);
     const currentTime = new Date().getTime()/1000;
 
     if (optionalKey === null)
-        if (currentTime > decode.exp) IsJwtTokenExpired = true;
+        if (currentTime > decode.payload.exp) IsJwtTokenExpired = true;
     else
-        if (decode.hasOwnProperty(optionalKey))
-            if (currentTime > decode.get(optionalKey)) IsJwtTokenExpired = true;
+        if (decode.payload.hasOwnProperty(optionalKey))
+            if (currentTime > decode.payload.get(optionalKey)) IsJwtTokenExpired = true;
         else
             throw new Error('Invalid optionalKey');
     
     return IsJwtTokenExpired;
+}
+
+const decodeToken = (token) =>{
+    let header = {};
+    let payload = {};
+    try{
+        [ header, payload ] = token.split('.').map( (encoded_string,index) => index != 2 ? JSON.parse(atob(encoded_string)) : '{}' );
+    }
+    catch(err){
+        throw new Error('Invalid JWT token');
+    }
+    return { header: header, payload: payload };
 }
